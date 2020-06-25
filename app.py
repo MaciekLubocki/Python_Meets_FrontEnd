8b243b0cd4940020748e31e302cf9ff35d6edb08
Maciek Lubocki <mail2maciek@gmail.com>
2020-06-25 19:43:36 +0200

    Code update

--- app.py
+++ app.py
@@ -2,27 +2,26 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+
 app = Flask(__name__)
+
 
 @app.route('/mypage/me.html', methods=['GET'])
 def message_my_page():
     print("I am written to the file")
     return render_template("/mypage/me.html")
-if __name__ == '__main__':
-    app.run()
+
 
 @app.route('/mypage/contact.html', methods=['GET', 'POST'])
 def message_contact_page():
-   items = ["mail:mail2maciek@gmail.com", "phone: 111-122-133"]
-   if request.method == 'GET':
-       print("We received GET")
-       return render_template("/mypage/contact.html", items=items)
-   elif request.method == 'POST':
-       print("We received POST")
-       print(request.form)
-       return redirect("/mypage/contact.html")
+    items = ["mail:mail2maciek@gmail.com", "phone: 111-122-133"]
+    if request.method == 'GET':
+        print("We received GET")
+        return render_template("/mypage/contact.html", items=items)
+    elif request.method == 'POST':
+        print("We received POST")
+        print(request.form)
+        return redirect("/mypage/contact.html")
 
-   if __name__ == '__main__':
-      app.run()
-
-
+    if __name__ == '__main__':
+        app.run()
